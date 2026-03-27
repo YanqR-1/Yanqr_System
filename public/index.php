094e40e96b8f8e29cf2dd011072ab604e246673a
YanqR-1 <giancant3@gmail.com>
2026-03-27 22:15:20 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
-<!--Kini ang entry point sa application.Nag handle siya sa pag route sa mga requests ngadto
-sa sakto nga controller ug method base
 <?php
 session_start();
 
 $url = isset($_GET['url']) ? $_GET['url'] : 'auth/login';
 $url = rtrim($url, '/');
 $url = explode('/', $url);
-
-$controllerName = ucfirst($url[0]) . 'Controller';
-$methodName = isset($url[1]) ? $url[1] : 'index';
 
 $controllerMap = [
     'auth' => 'AuthController',
@@ -19,17 +14,23 @@ $controllerMap = [
     'like' => 'LikeController'
 ];
 
-$controllerKey = $url[0];
-$controllerClass = isset($controllerMap[$controllerKey]) ? $controllerMap[$controllerKey] : $controllerName;
+$controllerKey = isset($url[0]) ? $url[0] : 'auth';
+$controllerClass = isset($controllerMap[$controllerKey]) ? $controllerMap[$controllerKey] : ucfirst($controllerKey) . 'Controller';
+$method = isset($url[1]) ? $url[1] : 'index';
+$params = array_slice($url, 2);
 
-$controllerFile = 'C:\\Xampp\\htdocs\\yanqr_system\\app\\controllers\\' . $controllerClass . '.php';
+$controllerFile = __DIR__ . '/../app/controllers/' . $controllerClass . '.php';
 
 if (file_exists($controllerFile)) {
     require_once $controllerFile;
     $controller = new $controllerClass();
     
-    if (method_exists($controller, $methodName)) {
-        $controller->$methodName();
+    if (method_exists($controller, $method)) {
+        if (!empty($params)) {
+            call_user_func_array([$controller, $method], $params);
+        } else {
+            $controller->$method();
+        }
     } else {
         echo "404 - Method not found";
     }
