9b1bc3ddd6def01d54dc9edb9ad2e3a1f242f724
YanqR-1 <giancant3@gmail.com>
2026-03-27 22:14:20 +0800

    Update footer.php

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,5 +1,5 @@
     </main>
-    <!-- para ni sa pinaka ubos na area like sa log in sa SMCC Portal katong "22026 © SMCC" -->
+    
     <?php if (isset($_SESSION['user_id'])): ?>
     <div class="quick-post">
         <button onclick="window.location.href='/yanqr_system/public/post/create'" class="quick-post-btn">
@@ -8,6 +8,7 @@
     </div>
     <?php endif; ?>
     
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/yanqr_system/public/assets/js/app.js"></script>
 </body>
 </html>
